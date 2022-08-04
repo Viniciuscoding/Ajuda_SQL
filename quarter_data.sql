86c9275e686755ef052f2b7fc3d47d71c34d60e9
Viniciuscoding <vin.idem@gmail.com>
2022-08-04 18:15:52 -0400

    Update quarter_data.sql

--- quarter_data.sql
+++ quarter_data.sql
@@ -1,5 +1,7 @@
-# SQL Query that finds date from the current date to a quarter ago.
+/*
+SQL Query that finds date from the current date to a quarter ago.
+*/
 
 SELECT * FROM table
 WHERE DATE(date_column) <= CURRENT_DATE() AND DATE(date_column) >= CURRENT_DATE() - 91
-ODER BY date_column
+ORDER BY date_column
